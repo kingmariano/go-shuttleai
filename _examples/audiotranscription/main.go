962c0ddf81e2797ade4ece5da8a36b5f4209d99f
Charles Ozochukwu <charlesozochukwu2004@gmail.com>
2024-03-04 03:26:33 +0100

    updated _examples code

--- _examples/audiotranscription/main.go
+++ _examples/audiotranscription/main.go
@@ -9,7 +9,7 @@ import (
 func main(){
     client := shuttleai.NewClient(os.getenv("OXYGEN_API_TOKEN"))
 	response, err := client.TranscribeAudio(context.Background(), &shuttleai.AudioTranscribeRequest{
-		Audio: []byte{"audio.mp3"}
+		File: []byte{"audio.mp3"},
 	})
 	if err != nil{
 		fmt.Println(err)
