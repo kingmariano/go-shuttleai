fbfdb640d238d51c07639268e923f9377f489b47
Charles Ozochukwu <charlesozochukwu2004@gmail.com>
2024-03-04 03:23:57 +0100

    restructured code

--- audioTranscription.go
+++ audioTranscription.go
@@ -1,35 +1,28 @@
 package shuttle
 
 import (
-	"bytes"
 	"context"
-	"mime/multipart"
-	"fmt"
-	"io"
 	"encoding/json"
 )
 
 type AudioTranscribeRequest struct {
-	Audio []byte 
+	File []byte `json:"file"`
 }
 type  AudioTranscribeResponse struct {
 	Text string `json:"text"`
 }
 func (sh *ShuttleClient) TranscribeAudio(ctx context.Context, req *AudioTranscribeRequest) (*AudioTranscribeResponse, error){
-	var requestBody bytes.Buffer
-    writer := multipart.NewWriter(&requestBody)
-	part, err := writer.CreateFormFile("file", "audio.mp3")
-    if err != nil {
-        return nil,  fmt.Errorf("Error creating form file: %v", err)
+	requestBody := map[string]interface{}{
+        "file": req.File,
     }
-	if _, err := io.Copy(part, bytes.NewReader(req.Audio)); err != nil {
-        return nil, fmt.Errorf("Error copying file: %v", err)
-    }
-	if err := writer.Close(); err != nil {
-        return nil, fmt.Errorf("Error closing writer: %v", err)
-    }
-	body, err := sh.post(ctx,"/v1/audio/transcriptions", "multipart/form-data", &requestBody)
-	if err != nil{
+
+    rBody, err := json.Marshal(requestBody)
+	if err != nil {
+		return nil, err
+	}
+	
+	body, err := sh.post(ctx, "/v1/audio/transcriptions", "multipart/form-data", rBody)
+	if err != nil {
 		return nil, err
 	}
 	audioTranscribeResponse := AudioTranscribeResponse{}
